5d704742bf44dcd7c36e0a621c4ec117df08b15b
dd0sxx <contacttheotelonis@gmail.com>
2021-08-10 15:24:44 -0400

    added function bodies to lock and cancle functions

--- contracts/Crowdfundr.sol
+++ contracts/Crowdfundr.sol
@@ -5,42 +5,81 @@ pragma solidity ^0.8.4;
         // creators can call createProject
 
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-/// @title Crowdfundr Project
+/// @title Project
 /// @notice Will serve as a class for each Project instance
-contract Project {
+contract Project is Ownable {
 
     using SafeMath for uint;
 
     mapping(address => uint) public balances;
-    address public owner;
     uint public expirationDate;
     uint public totalFunds;
     uint public goal;
     bool public locked;
-    bool public cancled;
+    bool public success;
 
-
-    /// @notice  for creators to lock their project in order to withdraw funds, or for users to lock if the goal has not been met after 30 days
-    function lock (string memory _name) public {
-
+    constructor(uint _daysToExpiration) {
+        expirationDate = block.timestamp + _daysToExpiration;
     }
 
-       /// @notice  for creators to withdraw funds after the goal has been met, or for contributors to withdraw their funds if the goal has not been met
-    function withdraw (string memory _name) public {
+    modifier isUnlocked {
+        require(!locked, 'the project is currently locked');
+        _;
+    }
+    modifier isLocked {
+        require(locked, 'the project is currently unlocked');
+        _;
+    }
+
+    event cancled (bool cancled, address owner);
+
+    /// @notice for creators to lock their project in order to withdraw funds
+    function lockOwner (string memory _name) public onlyOwner isUnlocked {
+        require(totalFunds >= goal);
+        locked = true;
+        success = true;
+    }
+
+    /// @notice for creators to cancle the project
+    function cancle (string memory _name) public onlyOwner isUnlocked {
+        require(block.timestamp < expirationDate);
+        success = false;
+        locked = true;
+        emit cancled(true, msg.sender);
+    }
+
+    /// @notice for users to lock if the goal has not been met after 30 days
+    function lockContributor (string memory _name) public isUnlocked {
+        require(block.timestamp > expirationDate);
+        require(totalFunds < goal);
+        locked = true;
+        success = false;
+    }
+
+
+
+    /// @notice  for contributors to withdraw their funds if the goal has not been met
+    function withdrawContributor (string memory _name) public isLocked {
+        require(!success, 'crowdfundr was unsuccessful');
+    }
+
+    /// @notice  for creators to withdraw funds after the goal has been met
+    function withdrawOwner (string memory _name) public onlyOwner isLocked {
+        require(success, 'crowdfundr was unsuccessful');
 
     }
 
     /// @notice users can contribute to the Project
-    fallback () external payable  {
-        require(msg.value > 0.01 ether);
-        if (balances[msg.sender] > 0) {
-            balances[msg.sender] = balances[msg.sender].add(msg.value);
-        } else {
-            balances[msg.sender] = msg.value;
-        }
-    }
+    fallback () external payable {
+        require(msg.value >= 0.01 ether, 'value was less than 0.01 ether');
+        require(!locked, 'project is locked');
+        require(totalFunds < goal, 'the project has already met its goal');
+        totalFunds = totalFunds.add(msg.value);
+        balances[msg.sender] = balances[msg.sender].add(msg.value);
 
+    }
 }
 
 /// @title Crowdfundr
